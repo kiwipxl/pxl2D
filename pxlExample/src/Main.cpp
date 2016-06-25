a72bc6c5b72ce352fa60bcfa5ddf5b893020781c
FlynnsPixel <flynnspixel@gmail.com>
2016-06-25 14:47:22 +1000

    fixed untracked files

--- pxlExample/src/Main.cpp
+++ pxlExample/src/Main.cpp
@@ -1,6 +1,8 @@
 #include <PXL.h>
 #include <ctime>
 
+using namespace pxl;
+
 //temporary to_string function until one has been added to pxl
 template <typename T> std::string to_string(const T& n) {
 	std::ostringstream stm;
@@ -19,8 +21,8 @@ int main(int argc, char* args[]) {
 	float fps = 60;
 	float ms_per_frame = 1000 / fps;
 	bool quit = false;
-	Timer start_time;
-	Timer start_second_time;
+	sys::Timer start_time;
+	sys::Timer start_second_time;
 	int frame_counter = 0;
 	long average_time = 0;
 	int average_count = 0;
@@ -49,66 +51,70 @@ int main(int argc, char* args[]) {
     result = time.end();
     std::cout << "r2: " << result << "\n";*/
 
-	Window window(480, 800, "PXL Example Project");
+    sys::Window window(480, 800, "PXL Example Project");
     init();
 
-    Batch batch = Batch(&window);
+    graphics::Batch batch = graphics::Batch(&window);
 
-    print << "loading assets...\n";
+    sys::print << "loading assets...\n";
 
-	Texture arena_texture;			arena_texture.create_texture("assets/arena.png");
-	Texture puck_texture;			puck_texture.create_texture("assets/puck.png");
-	Texture p1paddle_texture;		p1paddle_texture.create_texture("assets/p1paddle.png");
-    Texture p2paddle_texture;		p2paddle_texture.create_texture("assets/p2paddle.png");
-    Texture test_cat_texture;       test_cat_texture.create_texture("assets/lol.png");
+    graphics::Texture arena_texture;		arena_texture.create_texture("assets/arena.png");
+    graphics::Texture puck_texture;			puck_texture.create_texture("assets/puck.png");
+    graphics::Texture p1paddle_texture;		p1paddle_texture.create_texture("assets/p1paddle.png");
+    graphics::Texture p2paddle_texture;		p2paddle_texture.create_texture("assets/p2paddle.png");
+    graphics::Texture test_cat_texture;     test_cat_texture.create_texture("assets/lol.png");
 
-	print << "loaded assets. loading fonts...\n";
+	sys::print << "loaded assets. loading fonts...\n";
 
 	Vec2 window_center(window.get_width() / 2, window.get_height() / 2);
 
-	struct Paddle : public Sprite {
+	struct Paddle : public graphics::Sprite {
 
 		Vec2 dest;
 		int touchid;
 		Vec2 center;
     };
 
-	Sprite puck(puck_texture);
+    graphics::Sprite puck(puck_texture);
 	Vec2 puck_speed;
 	puck.x = window.get_width() / 2;	puck.y = window.get_height() / 2;
 	puck.width = 64;					puck.height = 64;
     puck.z_depth = 1;
 
-    print << "loaded fonts. creating batch...\n";
+    sys::print << "loaded fonts. creating batch...\n";
 
-	print << "created batch. checking joysticks...\n";
+	sys::print << "created batch. checking joysticks...\n";
 
-	int num = num_joysticks();
-	if (num > 0) {
-		Joystick* j = get_joystick(0);
-		j->activate();
-	}
+    {
+        using namespace input;
+	   
+       /* int num = num_joysticks();
+	    if (num > 0) {
+		    Joystick* j = get_joystick(0);
+		    j->activate();
+	    }*/
+    }
 
-	print << "checked joysticks. running loop...\n";
+	sys::print << "checked joysticks. running loop...\n";
 
 	start_second_time.start();
 	while (!quit) {
 		start_time.start();
 
-		Event e;
+		sys::Event e;
 		while (window.poll_event(e)) {
 			if (e.type == EVENT_QUIT) {
 				quit = true;
-				print << "am quitting\n";
+				sys::print << "am quitting\n";
 				break;
 			}
 		}
 
-		start_timer();
+		sys::start_timer();
 
 		t += .5f;
-		set_clear_colour(0, 0, 0, 1);
-        clear();
+        graphics::set_clear_colour(0, 0, 0, 1);
+        graphics::clear();
 
         puck.render(&batch);
 
@@ -123,14 +129,14 @@ int main(int argc, char* args[]) {
 		++average_count;
 		if (start_second_time.end() / 1000.0f >= 1000) {
 			if (seconds_elapsed % 2 == 0) {
-				print << "elapsed: " << average_time / average_count <<
+				sys::print << "elapsed: " << average_time / average_count <<
 					", ms: " << (average_time / average_count) / 1000.0f << "\n";
 				average_time = 0;
 				average_count = 0;
 			}
 			++seconds_elapsed;
 
-			print << "fps: " << frame_counter << "\n";
+			sys::print << "fps: " << frame_counter << "\n";
 
 			frame_counter = 0;
 			start_second_time.start();
